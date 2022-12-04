bd62ba00d5a9a8d6f5b7eff9f0b6da4acbcc7688
vladislavkiselovls <vladislav.kiselovls@gmail.com>
2022-12-04 20:27:32 +0200

    deploy

--- pages/data.js
+++ pages/data.js
@@ -3,20 +3,24 @@ import React, { useEffect, useState } from 'react';
 
 const data = () => {
 	const [data, setData] = useState([]);
+	const [loading, setLoading] = useState(false);
 
 	useEffect(() => {
 		fetchData();
 	}, []);
 
 	const fetchData = async () => {
+		setLoading(false);
 		const res = await axios.get('/api/data');
 
 		setData(res.data.data);
+		setLoading(true);
 	};
 
 	return (
 		<div style={{ padding: '15px' }}>
 			<div style={{ fontSize: '18px' }}>Data</div>
+			{loading && 'Loading...'}
 			{data?.map((item, idx) => (
 				<div style={{ marginTop: '10px' }} key={idx}>
 					<div>Login: {item.login}</div>
